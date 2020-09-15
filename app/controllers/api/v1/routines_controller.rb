8e6073ccc207c00a4de9b43fadd8349212068511
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-09-15 19:29:15 -0400

    create routines controller && update routes

---
+++ app/controllers/api/v1/routines_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::RoutinesController < ApplicationController
+    def index
+    @routines = Routine.all.order(:created_at)
+    render json: @routines, status: 200
+  end
+
+  def create
+    @routine = Routine.create(routine_params)
+    render json: @routine, status: 201
+  end
+
+  def destroy
+    routine = Routine.find(params[:id])
+    render json: { routineId: routine.id }, status: 200 if routine.destroy
+  end
+
+  def update
+    @routine = Routine.find(params[:id])
+    # if @routine.update(routine_params)
+    #   render json: @routine, status: 200
+
+    #  @routine.update(routine_params)
+    if @routine.save
+      render json: @routine, status: :accepted
+    else
+      render json: { errors: @routine.errors.full_messages }, status: :unprocessible_entity
+      # end
+    end
+  end
+
+  private
+
+  def routine_params
+    params.require(:routine).permit(:type, :name, :duration, :distance)
+  end
+
+  #   def routine_params
+  #     params.permit(:title, :content)
+  #   end
+
+  #   def find_routine
+  #     @routine = Routine.find(params[:id])
+  #   end
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :workouts # , only: [:index, :update], :routine
+      resources :workouts, :routines # , only: [:index, :update], :routine
     end
   end
   # end
