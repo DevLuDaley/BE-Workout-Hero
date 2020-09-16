18d91433017896c59b8a99ed2ff3e9f43f634881
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-09-16 12:03:11 -0400

    create workout_serializer

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :name, :workout_type, :distance, :duration
+  attributes :id, :name, :workout_type, :distance, :duration, :created_at
   # has_many :routines
   # attributes :name
 end
