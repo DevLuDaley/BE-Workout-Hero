cab65412dcbd50f816eeb037fe322ee9562b515f
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-09-16 12:04:38 -0400

    add test route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :api do
     namespace :v1 do
       resources :workouts, :routines # , only: [:index, :update], :routine
     end
   end
+  # end
+  get '/test', to: 'application#test'
   # end
 end
