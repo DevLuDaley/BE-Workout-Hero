905645c7101dc0ec758cc6d78f445bc72c5969e1
devludaley <daleyluis@gmail.com>
2020-09-27 04:46:13 -0400

    remove commented code

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -22,18 +22,10 @@ class Api::V1::RoutinesController < ApplicationController
     # binding.pry
     @routine.workouts.build(workout_type: params[:workout_type], workout_name: params[:workout_name], distance: params[:distance], duration: params[:duration])
 
-    # @routine = Routine.find(params[:id]).workouts.create(
-    # params[:workout_type], params[:routine_name], params[:distance], params[:duration]
-    # )
-    # if @routine.update(routine_params)
-    #   render json: @routine, status: 200
-    #  ? 02 > r.workouts.create(workout_type:"Cardio", routine_name: "Sprinting", distance: "2", duration: "4")
-    #  @routine.update(routine_params)
     if @routine.save
       render json: @routine, status: :accepted
     else
       render json: { errors: @routine.errors.full_messages }, status: :unprocessible_entity
-      # end
     end
   end
 
@@ -46,14 +38,4 @@ class Api::V1::RoutinesController < ApplicationController
   def update_params
     params.require(:routine).permit(:routine_name, :type, :workout_name, :duration, :distance, :workouts)
   end
-
-  #   def routine_params
-  #     params.permit(:title, :content)
-  #   end
-
-  #   def find_routine
-  #     @routine = Routine.find(params[:id])
-  #   end
 end
-
-# end
