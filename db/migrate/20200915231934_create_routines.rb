dcc5ff8835dcc6e14af2132a0c895c2061574fbe
devludaley <daleyluis@gmail.com>
2021-01-06 16:01:46 -0500

    Dump then reload migration files

--- db/migrate/20200915231934_create_routines.rb
+++ db/migrate/20200915231934_create_routines.rb
@@ -4,6 +4,7 @@ class CreateRoutines < ActiveRecord::Migration[6.0]
   def change
     create_table :routines do |t|
       t.string :routine_name
+      # t.string :sugar
 
       t.timestamps
     end
