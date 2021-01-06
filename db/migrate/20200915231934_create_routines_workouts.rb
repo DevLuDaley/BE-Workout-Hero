df033e8251d0b9b06042b47993ce03be2380f6bc
devludaley <daleyluis@gmail.com>
2021-01-06 12:54:08 -0500

    uncomment t.references in 'create_routines_workouts.rb'

--- db/migrate/20200915231934_create_routines_workouts.rb
+++ db/migrate/20200915231934_create_routines_workouts.rb
@@ -1,10 +1,10 @@
 class CreateRoutinesWorkouts < ActiveRecord::Migration[6.0]
   def change
     create_table :routines_workouts do |t|
-      # t.references :routine, null: false, foreign_key: true
-      # t.references :workout, null: false, foreign_key: true
-      t.bigint "routine_id", null: false
-      t.bigint "workout_id", null: false
+      t.references :routine, null: false, foreign_key: true
+      t.references :workout, null: false, foreign_key: true
+      # t.bigint "routine_id", null: false
+      # t.bigint "workout_id", null: false
     end
   end
 end
