79cecdb3dd529485921dee2cb5bc7550967654d9
devludaley <daleyluis@gmail.com>
2021-01-06 12:49:07 -0500

    Update workout.rb

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ApplicationRecord
-    # has_and_belongs_to_many :routines
+    has_and_belongs_to_many :routines
 end
 
 # rails g migration CreateWorkoutsRoutines workout:references routine:references
