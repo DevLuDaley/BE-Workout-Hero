a550be526a9694824060f9126d9c7b13ca6ba6a3
devludaley <daleyluis@gmail.com>
2021-01-06 17:29:29 -0500

    Create 20210106222741_create_join_table.rb

---
+++ db/migrate/20210106222741_create_join_table.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateJoinTable < ActiveRecord::Migration[6.0]
+  def change
+    create_join_table :routines, :workouts 
+    # do |t|
+      # t.index [:_id, :_id]
+      # t.index [:_id, :_id]
+    # end
+  end
+end
