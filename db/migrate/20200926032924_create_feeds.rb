b1a33ca94a56db61048f068d6d7f1f7d816cbe0b
devludaley <daleyluis@gmail.com>
2021-01-06 10:29:30 -0500

    Create 20200926032924_create_feeds.rb to fix migration ID that delete after migration file was deleted

---
+++ db/migrate/20200926032924_create_feeds.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateRoutines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :feeds do |t|
+      t.string :feed_name
+
+      t.timestamps
+    end
+  end
+end
