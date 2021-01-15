8659aa81d4a5a5e1e797aaaf35ce2a719fde3bae
devludaley <daleyluis@gmail.com>
2021-01-15 12:58:45 -0500

    bugfix- update triggers duplicate routine info

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -20,27 +20,30 @@ class Api::V1::RoutinesController < ApplicationController
   def update
     @routine = Routine.find(params[:id])
     # binding.pry
+    @routine.update(routine_params)
 
-    workout_type, workout_name, distance, duration = params.values_at(
-      :workout_type,
-      :workout_name,
-      :workout_distance,
-      :workout_duration
-    )
-
-    @routine.workouts.build(
-      workout_type: workout_type,
-      workout_name: workout_name,
-      distance: distance,
-      duration: duration
-    )
+    # workout_type, workout_name, distance, duration = params.values_at(
+    #   :workout_type,
+    #   :workout_name,
+    #   :workout_distance,
+    #   :workout_duration
+    # )
+    
+    # @routine.workouts.build(
+    #   workout_type: workout_type,
+    #   workout_name: workout_name,
+    #   distance: distance,
+    #   duration: duration
+    # )
     # @routine.workouts.build(workout_type: params[:workout_type], workout_name: params[:workout_name], distance: params[:distance], duration: params[:duration])
+    @routine.save
 
     if @routine.save
       render json: @routine, status: :accepted
     else
       render json: { errors: @routine.errors.full_messages }, status: :unprocessible_entity
     end
+    binding.pry
   end
 
   private
@@ -49,7 +52,7 @@ class Api::V1::RoutinesController < ApplicationController
     params.require(:routine).permit(:routine_name)
   end
 
-  def update_params
-    params.require(:routine).permit(:routine_name)
-  end
+  # def update_params
+  #   params.require(:routine).permit(:routine_name)
+  # end
 end
