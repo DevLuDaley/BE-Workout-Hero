7fc319a6d883e152a82ff598b03199621f81c1ab
devludaley <daleyluis@gmail.com>
2021-01-21 16:58:42 -0500

    config/routes.rb => add a catchall for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 # root 'welcome#main'
+# get '*path', to: 'application#fallback_index_html', constraints: ->(request) do
+#     !request.xhr? && request.format.html?
+#   end
 
 Rails.application.routes.draw do
   root to: 'welcome#index'
 # root 'welcome#index'
+
+
   namespace :api do
     namespace :v1 do
       resources :workouts, :routines
     end
+  end
+
+    get '*path', to: 'application#fallback_index_html', constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
 end
