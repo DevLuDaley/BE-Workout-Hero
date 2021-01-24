105790fd7992744c9ecbd55482cdd62882bd047c
devludaley <daleyluis@gmail.com>
2021-01-24 05:59:52 -0500

    app/controllers/api/v1/routines_controller.rb => create a conditional that will only create a new workout if workout fields/info is supplied by react

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -19,8 +19,9 @@ class Api::V1::RoutinesController < ApplicationController
 
   def update
     @routine = Routine.find(params[:id])
-    binding.pry
+    # binding.pry
     @routine.update(routine_params)
+    if params.include?('workout_name')
 
     workout_type, workout_name, distance, duration = params.values_at(
       :workout_type,
@@ -35,6 +36,10 @@ class Api::V1::RoutinesController < ApplicationController
       distance: distance,
       duration: duration
     )
+
+    else
+      @routine.save
+    end
     # @routine.workouts.build(workout_type: params[:workout_type], workout_name: params[:workout_name], distance: params[:distance], duration: params[:duration])
     # @routine.save
 
