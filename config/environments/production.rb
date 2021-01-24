b0a39bfbb0b5b976ce74ca18832c1828776bca7e
devludaley <daleyluis@gmail.com>
2021-01-23 20:23:47 -0500

    config/environments/production.rb => comment config.assets && config.serve...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.assets.complie = true
-config.serve_static_assets = true
+#   config.assets.complie = true
+# config.serve_static_assets = true
 end
