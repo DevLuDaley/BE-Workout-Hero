6a1f70a6e9b293ec98d68589b6946ef948d5324b
devludaley <daleyluis@gmail.com>
2021-02-02 14:57:26 -0500

    add workout logic

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -19,6 +19,7 @@ class Api::V1::RoutinesController < ApplicationController
       @workout_to_delete = @workoutlist.select { |w| w.id == params[:workout_id_to_delete] }
       # can = @routine.workouts.select { |w| w.id == params[:workout_id_to_delete] }
       @routine.workouts.destroy(@workout_to_delete)
+
       render json: { routineId: params[:id], workoutId: params[:workout_id_to_delete] }, status: 200
       # render json: { routineId: @routine.id, workoutId: params[:workout_id_to_delete] }, status: 200
       # @workout_to_delete.destroy
@@ -34,32 +35,78 @@ class Api::V1::RoutinesController < ApplicationController
     @routine = Routine.find(params[:id])
     # binding.pry
     @routine.update(routine_params)
-    if params.include?('workout_name')
-
-    workout_type, workout_name, distance, duration = params.values_at(
-      :workout_type,
-      :workout_name,
-      :distance,
-      :duration
-    )
     
-    @routine.workouts.build(
-      workout_type: workout_type,
-      workout_name: workout_name,
-      distance: distance,
-      duration: duration
-    )
+    if params.include?('update_workout')
+      @workoutlist = @routine.workouts
+      @workout = @workoutlist.find { |w| w.id == params[:workout_id].to_i}
+      # @workout.update(workout_params)
+      # @workout.update(
+      #   {
+      #     id: params[:workout_id],
+      #     workout_type: params[:workout_type],
+      #     workout_name: params[:workout_name],
+      #     distance: params[:distance],
+      #     duration: params[:duration]
+      #   }
+      # )
+      # id, workout_type, workout_name, distance, duration = params.values_at(
+      #   :workout_id,
+      #   :workout_type,
+      #   :workout_name,
+      #   :distance,
+      #   :duration
+      # )
+      
+      # @routine.workouts.update(workout: @workout)
+      @workout.update(
+        {
+          # id: params[:workout_id],
+          # workout: @workout,
+          workout_type: params[:workout_type],
+          workout_name: params[:workout_name],
+          distance: params[:distance],
+          duration: params[:duration]
+        }
+      )
+      # @workout.save
+      # render json: @routine, status: :accepted
+      # binding.pry
 
+    # else if params.include?('workout_name')
+    else if params.include?('create_workout')
+      workout_type, workout_name, distance, duration = params.values_at(
+        :workout_type,
+        :workout_name,
+        :distance,
+        :duration
+      )
+      @routine.workouts.build(
+        workout_type: workout_type,
+        workout_name: workout_name,
+        distance: distance,
+        duration: duration
+      )
     else
       @routine.save
     end
+    end
     # @routine.workouts.build(workout_type: params[:workout_type], workout_name: params[:workout_name], distance: params[:distance], duration: params[:duration])
     # @routine.save
+    # if @workout.save
+    #   render json: @workout, status: :accepted
+    #       else
+    #   render json: { errors: @workout.errors.full_messages }, status: :unprocessible_entity
+    # end
 
-    if @routine.save
+
+    if @workout.save && @routine.save
+      render json: {workout: @workout, routineId: @routine.id }, status: :accepted
+
+    else if @routine.save
       render json: @routine, status: :accepted
     else
       render json: { errors: @routine.errors.full_messages }, status: :unprocessible_entity
+    end
     end
     # binding.pry
   end
@@ -67,7 +114,11 @@ class Api::V1::RoutinesController < ApplicationController
   private
 
   def routine_params
-    params.require(:routine).permit(:routine_name)
+    params.require(:routine).permit(:routine_name, :id)
+  end
+
+  def workout_params
+    params.permit(:workout_id, :workout_name, :workout_type, :distance, :duration)
   end
 
   # def update_params
