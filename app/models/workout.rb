e26ef2be0647162cdba342f5065f4f57f7e73842
devludaley <daleyluis@gmail.com>
2021-02-13 22:45:51 -0500

    update routine:workout relationships

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,6 @@
 class Workout < ApplicationRecord
     has_and_belongs_to_many :routines
+    accepts_nested_attributes_for :routines
 end
 
 # rails g migration CreateWorkoutsRoutines workout:references routine:references
